6bb17de3b4c854f956d3053c1c92677eb638ed2c
Astaroth <avm725@gmail.com>
2020-12-25 07:04:15 +0500

    Task04[Myslivko]

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,9 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Formatter;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +12,20 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        double sum = 0.0;
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (inputStreamReader.ready()) {
+            stringBuilder.appendCodePoint(inputStreamReader.read());
+        }
+        String[] parsed = stringBuilder.toString().split("\\s");
+        for (String num : parsed) {
+            try {
+                sum += Double.parseDouble(num);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        System.out.println(new Formatter(Locale.US).format("%.6f", sum));
+
     }
 }
